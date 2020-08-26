32b56220fcb119884e3f4ee7fedfd1747ff17374
halliwell2046 <halliwell2046@yahoo.com>
2020-08-26 19:03:29 -0400

    corrected error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Photo from './Components/Photo';
+import nasalogo from './assets/nasalogo.png';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img src={nasalogo} />
      <Photo />
     </div>
   );
--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -6,31 +6,21 @@ import PhotoDis from './PhotoDis';
 export default function NasaPhoto() {
     const [photo, setPhoto] = useState([]);
 
+    useEffect(() => {
+        axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=5WpADO5JTrQSV2VJqheqq90fRbEyfMagbdbWSIgj`)
+        .then((res) => {
+            console.log('Res: ', res);
+            setPhoto(res.data);
+        })
+        .catch((err) => {
+            console.log('An error occurred: ', err);
+        });
+    }, []);
 
-useEffect(() => {
-    axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=5WpADO5JTrQSV2VJqheqq90fRbEyfMagbdbWSIgj`)
-    .then((res) => {
-        console.log('Response: ', res);
-        setPhoto(res.data);
-    })
-    .catch((err) => {
-        console.log("An Error has occured: ", err );
-    });
-}, []);
-
-return(
-    <div>
-        {photo.map((image) => {
-            return ( 
-                <PhotoDis
-                copy={image.copywright}
-                title={image.title}
-                about={image.description}
-                date={image.date}
-                />
-            )
-        })}
-    </div>
-)
+    return (
+      <div>
+          <img src={photo.hdurl} />
+      </div>
+    );
 }
